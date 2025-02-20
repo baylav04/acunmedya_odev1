00f078febd916df3a57fdd4a822816c1b19669a9
kadriye <baylavkadriye@gmail.com>
2025-02-20 22:43:09 +0300

    acunmedya_odev1

---
+++ acunmedya_odev1.cs
@@ -0,0 +1,19 @@
+﻿//ODEV1
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı girin: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+            Console.WriteLine("Sayı pozitiftir.");
+        else if (num < 0)
+            Console.WriteLine("Sayı negatiftir.");
+        else
+            Console.WriteLine("Sayı sıfırdır.");
+    }
+}
